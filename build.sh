eff8310968d12c52d15f29da442c9cc835cd2e8e
Chris Sexton <chris.sexton@gmail.com>
2014-12-21 18:47:11 -0500

    make build quieter

--- build.sh
+++ build.sh
@@ -6,6 +6,3 @@ if [ ! -d src/typings ]; then
 fi
 
 ./node_modules/.bin/tsc --target es5 -m amd --out testApl.js src/testApl.ts
-
-echo "You may test the interpreter by using:"
-echo "  node testApl <list of string expressions>"
