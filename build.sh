1a550b0687338bf7a27bef516266e611c02d38db
Chris Sexton <chris.sexton@gmail.com>
2014-12-21 17:47:39 -0500

    Move source to a better spot

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 npm install
 ./node_modules/.bin/tsd reinstall
 ./node_modules/.bin/tsd rebundle
-./node_modules/.bin/tsc --target es5 -m amd --out testApl.js testApl.ts
+./node_modules/.bin/tsc --target es5 -m amd --out testApl.js src/testApl.ts
 
 echo "You may test the interpreter by using:"
 echo "  node testApl <list of string expressions>"
